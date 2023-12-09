fe8a589bd15ed1b33b6806308574bbea61468802
anildalar <dollorinfotech@gmail.com>
2023-12-09 09:11:55 +0530

    new

---
+++ function.py
@@ -0,0 +1,13 @@
+# 1 Function defination
+# Function defination is one time process
+def addition(formalarg1,formalarg2):
+    result = formalarg1 + formalarg2
+    print(f"The addition of {formalarg1} and {formalarg2} is {result}")
+
+# 2 Function Calling/Invoking
+# Function calling is many time process
+addition(5,5) # actualarg1=5,actualarg2=5
+addition(10,5)
+addition(20,20)
+
+
